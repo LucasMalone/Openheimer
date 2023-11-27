ac74864ca4be28148cc818a025a9b07f8517ff74
unknown <lucas.oliveira1720@etec.sp.gov.br>
2023-11-27 15:17:21 -0300

    first commit

---
+++ Oppenheimer/script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({ reset: true});
+
+
+sr.reveal('.quemFoi', {duration:2000});
+sr.reveal('.biografia1' ,{duration:2000});
+sr.reveal('.Guerra', {duration:2000});
+sr.reveal('.biografia2' ,{duration:2000});
+sr.reveal('.teste', {duration:2000});
+sr.reveal('.biografia3' ,{duration:2000});
+sr.reveal('.hiroshima', {duration:2000});
+sr.reveal('.biografia4' ,{duration:2000});
